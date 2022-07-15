e5113d4f48d02c119d4a55f8131514b3b3b05399
Jesús Hernández <cjesus02hdz@gmail.com>
2022-07-14 23:13:44 -0500

    delete and create collection of data api

--- src/controllers/posts.controller.js
+++ src/controllers/posts.controller.js
@@ -21,6 +21,43 @@ export const createPostClima = async (req, res) => {
     axios(config)
         .then(function (responseClima) {
             console.log(JSON.stringify(responseClima.data));
+			const {name, country, region, lat, lon, localtime} = responseClima.data.location
+			const {temperature, weather_code, weather_descriptions, wind_speed, wind_degree, wind_dir, pressure, precip, humidity, cloudcover, feelslike, uv_index, visibility, is_day} = responseClima.data.current
+
+			console.log(name, region, country)
+
+			const deleteClimaFn = async () => {
+				const eliminar = await Clima.deleteMany({})
+			}
+
+			deleteClimaFn()
+
+			const newClima = async () => {
+				new Clima({
+					city: name,
+					estado: region,
+					pais: country,
+					longitud: lon,
+					latitud: lat,
+					horalocal: localtime,
+					temperatura: temperature,
+					code: weather_code,
+					description: weather_descriptions,
+					velocidadViento: wind_speed,
+					gradosViento: wind_degree,
+					directionViento: wind_dir,
+					presion: pressure,
+					precipitacion: precip,
+					humedad: humidity,
+					nubusidad: cloudcover,
+					rayosUv: uv_index,
+					visibilidad: visibility,
+					isDay: is_day
+				}).save()
+			}
+			newClima()
+
+
         })
         .catch(function (error) {
             console.log(error);
@@ -34,7 +71,7 @@ export const createPostClima = async (req, res) => {
 
     // const text = req.body.text;
     const img = 'https://clima.lamaschingona.com.mx/img/clima.png'
-	const description = 'Holaa que k hac'
+	const description = 'Prueba hsfdhdfkdnjcisdicdjicdeoies'
 
 	var configg = {
 		method: "post",
@@ -42,15 +79,15 @@ export const createPostClima = async (req, res) => {
 		headers: {},
 	};
 
-	axios(configg)
-		.then(function (response) {
-			console.log(JSON.stringify(response.data));
-			res.status(200).json({res: 'Succes ok 200'})
-		})
-		.catch(function (error) {
-			console.log(error);
-			res.json({error})
-	});
+	// axios(configg)
+	// 	.then(function (response) {
+	// 		console.log(JSON.stringify(response.data));
+	// 		res.status(200).json({res: 'Succes ok 200'})
+	// 	})
+	// 	.catch(function (error) {
+	// 		console.log(error);
+	// 		res.json({error})
+	// });
 
 }
 
